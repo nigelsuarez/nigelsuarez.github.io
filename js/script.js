62240d8b4aefd0618e97fd650523854b013c4ecd
nigelsuarez <110112311+nigelsuarez@users.noreply.github.com>
2022-08-10 22:06:55 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,33 +6,22 @@ function clearTrans(a, b, c, d, e) {
   document.getElementById("project5").style.transform = e;
 }
 function webContent() {
-  document.getElementById("title1").innerHTML = "Website";
+  document.getElementById("title1").innerHTML = "WEBSITE";
   clearTrans("scale(1.05)", "none", "none", "none", "none");
-  document.getElementById("title1").style.backgroundColor = "rgb(228,79,38)";
-  document.getElementById("title1").style.color = "white";
 }
 function jsContent() {
-  document.getElementById("title1").innerHTML = "JavaScript";
+  document.getElementById("title1").innerHTML = "JAVASCRIPT";
   clearTrans("none", "scale(1.05)", "none", "none", "none");
-  document.getElementById("title1").style.backgroundColor = "rgb(247, 224, 24)";
-  document.getElementById("title1").style.color = "black";
 }
 function cppContent() {
   document.getElementById("title1").innerHTML = "C++";
   clearTrans("none", "none", "scale(1.05)", "none", "none");
-  document.getElementById("title1").style.backgroundColor = "rgb(1, 10, 91)";
-  document.getElementById("title1").style.color = "white";
 }
 function pyContent() {
-  document.getElementById("title1").innerHTML = "Python";
+  document.getElementById("title1").innerHTML = "PYTHON";
   clearTrans("none", "none", "none", "scale(1.05)", "none");
-  document.getElementById("title1").style.backgroundColor = "rgb(29, 34, 68)";
-  document.getElementById("title1").style.color = "rgb(254,209,66)";
 }
 function blendContent() {
-  document.getElementById("title1").innerHTML = "Blender";
+  document.getElementById("title1").innerHTML = "BLENDER";
   clearTrans("none", "none", "none", "none", "scale(1.05)");
-  document.getElementById("title1").style.backgroundColor =
-    "rgb(255, 193, 134)";
-  document.getElementById("title1").style.color = "rgb(254,109,1)";
 }
